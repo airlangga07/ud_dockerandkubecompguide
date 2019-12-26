9604e8d4ec3d05b7c55e6c492b60c5fbe6116436
Airlangga, Mikael <mikael.airlangga@gmail.com>
2019-12-27 00:45:06 +0800

    5-49. App Server Code

---
+++ visits/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const redis = require('redis');
+
+// initialize the express
+const app = express();
+
+// create client that will listen to the redis server
+const client = redis.createClient();
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+    client.get('visits', (err, visits) => {
+        res.send('Number of visits is ' + visits);
+        client.set('visits', parseInt(visits) + 1);
+    });
+});
+
+app.listen(8081, () => {
+    console.log('Listening on port 8081');
+})
