8bb31d3bae1b9720d06bd023bc1f3258ae2ba661
Airlangga, Mikael <mikael.airlangga@gmail.com>
2020-01-13 23:09:33 +0800

    Add changes?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.<br/>
-          Hi There!
+          Got Changed from the feature branch!
         </p>
         <a
           className="App-link"
